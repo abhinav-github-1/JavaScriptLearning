0971d49e1b09f74c58dd288f6dab2bab6bc67fc7
Kuwar Abhinav Singh <148185485+abhinav-github-1@users.noreply.github.com>
2025-09-15 08:20:19 +0000

    learned about comaprison of data types

---
+++ basic_01/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+//reason is that an equality check == and comaprisons >>=<<= work differently.
+//comparisons convert null to a number treating it as 0.
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//===(Strict check checks values and data types as well)
+console.log("2"===2);
