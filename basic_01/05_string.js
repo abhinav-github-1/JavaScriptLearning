42e4cfd743f2b37b66c69894e26730ccd5615d3a
Kuwar Abhinav Singh <148185485+abhinav-github-1@users.noreply.github.com>
2025-09-22 08:21:52 +0000

    strings implementation

---
+++ basic_01/05_string.js
@@ -0,0 +1,8 @@
+const name="abhinav"
+const repoCount=30
+
+//console.log(name+repoCount+"hello")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('bobby')
