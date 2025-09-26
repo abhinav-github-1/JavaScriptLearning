fb7c0271a1fd9ad1d922605db9b5906a0f16f41d
Kuwar Abhinav Singh <148185485+abhinav-github-1@users.noreply.github.com>
2025-09-26 07:28:58 +0000

    more functions of strings

--- basic_01/05_string.js
+++ basic_01/05_string.js
@@ -5,7 +5,7 @@ const repoCount=30
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName=new String('bobby')
+const gameName=new String('bobby-deol-sunny-deol')
 console.log(gameName[0]);
 console.log(gameName._proto_);
 console.log(gameName.length);
@@ -19,3 +19,12 @@ console.log(newString);
 const anotherString=gameName.slice(-8,4);
 console.log(anotherString);
 
+const newStringone="  abhinav  "
+console.log(newStringone);
+console.log(newStringone.trim());
+
+const url="https://abhinav.com/abhinav%20singh"
+console.log(url.replace('%20','-'));
+console.log(url.includes('abhi'));
+
+console.log(gameName.split('-'));
